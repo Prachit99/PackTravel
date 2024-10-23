0e16b993ef928cc67b06c2c1b683b10b81069c31
KoruptTinker <pisces1237@gmail.com>
2024-10-22 20:37:51 -0400

    Add: Error handling for google

--- services/google_maps/routes.py
+++ services/google_maps/routes.py
@@ -9,49 +9,55 @@ class Routes:
         self.api_key = api_key
 
     def __get_route_details__(self, slat: str, slong: str, dlat: str, dlong: str):
-        conn = client.HTTPSConnection(self.hostname)
-        payload = json.dumps({
-            "origin": {
-                "location": {
-                "latLng": {
-                    "latitude": slat,
-                    "longitude": slong
-                }
-                }
-            },
-            "destination": {
-                "location": {
-                "latLng": {
-                    "latitude": dlat,
-                    "longitude": dlong
-                }
-                }
-            },
-            "routeModifiers": {
-                "vehicleInfo": {
-                "emissionType": "GASOLINE"
-                }
-            },
-            "travelMode": "DRIVE",
-            "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
-            "extraComputations": [
-                "FUEL_CONSUMPTION"
-            ]
-        })
+        try: 
+            conn = client.HTTPSConnection(self.hostname)
+            payload = json.dumps({
+                "origin": {
+                    "location": {
+                    "latLng": {
+                        "latitude": slat,
+                        "longitude": slong
+                    }
+                    }
+                },
+                "destination": {
+                    "location": {
+                    "latLng": {
+                        "latitude": dlat,
+                        "longitude": dlong
+                    }
+                    }
+                },
+                "routeModifiers": {
+                    "vehicleInfo": {
+                    "emissionType": "GASOLINE"
+                    }
+                },
+                "travelMode": "DRIVE",
+                "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
+                "extraComputations": [
+                    "FUEL_CONSUMPTION"
+                ]
+            })
 
-        headers = {
-            'content-type': 'application/json',
-            'Content-Type': 'application/json',
-            'X-Goog-Api-Key': self.api_key,
-            'X-Goog-FieldMask': 'routes.distanceMeters,routes.duration,routes.routeLabels,routes.routeToken,routes.travelAdvisory.fuelConsumptionMicroliters'
-        }
+            headers = {
+                'content-type': 'application/json',
+                'Content-Type': 'application/json',
+                'X-Goog-Api-Key': self.api_key,
+                'X-Goog-FieldMask': 'routes.distanceMeters,routes.duration,routes.routeLabels,routes.routeToken,routes.travelAdvisory.fuelConsumptionMicroliters'
+            }
 
-        conn.request("POST", "/directions/v2:computeRoutes", payload, headers)
-        res = conn.getresponse()
-        data = res.read()
-        data = json.loads(data)
-        return {
-            "distance": int(data.get("routes", [])[0].get("distanceMeters", 0))/1000,
-            "fuel": int(data.get("routes", [])[0].get("travelAdvisory", {}).get("fuelConsumptionMicroliters", 0))/(1000*1000),
-        }
+            conn.request("POST", "/directions/v2:computeRoutes", payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            data = json.loads(data)
+            return {
+                "distance": int(data.get("routes", [])[0].get("distanceMeters", 0))/1000,
+                "fuel": int(data.get("routes", [])[0].get("travelAdvisory", {}).get("fuelConsumptionMicroliters", 0))/(1000*1000),
+            }
+        except:
+            return {
+                "distance": 0,
+                "fuel": 0,
+            }
 
