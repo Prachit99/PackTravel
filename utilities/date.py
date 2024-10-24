ab1b333e35a3168edfb0d25a03efe9ff079f5e6e
KoruptTinker <pisces1237@gmail.com>
2024-10-23 20:10:34 -0400

    Fix: Classmethod

--- utilities/date.py
+++ utilities/date.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 class DateUtils:
     @classmethod
-    def has_date_passed(date: str) -> bool: 
+    def has_date_passed(cls, date: str) -> bool: 
         given_date = datetime.strptime(date, "%Y-%m-%d").date()
         
         today = datetime.today().date()
